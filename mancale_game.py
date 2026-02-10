570298cbda4fe9e0094021ac92c9342d878f76ee
Morinbh <morinbh98@gmail.com>
2026-02-10 14:59:57 +0200

    1.1 Illegal hole test and inplement

--- mancale_game.py
+++ mancale_game.py
@@ -1,15 +1,12 @@
-from random import seed
-
-
 class Mancala:
     def __init__(self, hole: int, seeds: int):
-        self.hole = hole
         self.seeds = seeds
-        self.stores = 0
+        self.hole = [seeds] * hole + [0] + [seeds] * hole + [0]  # 2 rows + 2 stores
+        self.current_player = 1
 
     def initial_status(self):
-        pits = (self.seeds,) * self.hole
-        return (
-            *pits,
-            0,
-        ) * 2
+        return tuple(self.hole)
+
+    def play(self, hole):
+        if hole < 0 or hole >= len(self.hole):
+            raise IndexError("Hole number is out of range.")
--- tests_file_mancala_game.py
+++ tests_file_mancala_game.py
@@ -1,3 +1,11 @@
+from mancale_game import Mancala
+
+
 def test_initial_status():
     mancala1 = Mancala(6, 4)
     assert mancala1.initial_status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+
+
+def test_illigal_hole():
+    mancala1 = Mancala(6, 4)
+    assert mancala1.play(5) is not IndexError
