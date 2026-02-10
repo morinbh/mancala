f3f644f891898687b1a360f5127852d0e3f70250
Morinbh <morinbh98@gmail.com>
2026-02-10 15:14:18 +0200

    1.2 Simple move test

--- tests_file_mancala_game.py
+++ tests_file_mancala_game.py
@@ -3,9 +3,18 @@ from mancale_game import Mancala
 
 def test_initial_status():
     mancala1 = Mancala(6, 4)
-    assert mancala1.initial_status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    assert mancala1.status() == (4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0)
 
 
-def test_illigal_hole():
+def test_illegal_hole():
     mancala1 = Mancala(6, 4)
     assert mancala1.play(5) is not IndexError
+
+
+def test_simple_move():
+    mancala1 = Mancala(6, 4)
+    mancala1.play(0)  # Player 1 plays from hole 0
+    assert mancala1.status() == (0, 5, 5, 5, 5, 4, 0, 4, 4, 4, 4, 4, 4, 0)
+    mancala2 = Mancala(6, 4)
+    mancala2.play(1)  # Player 1 plays from hole 1
+    assert mancala2.status() == (4, 0, 5, 5, 5, 5, 0, 4, 4, 4, 4, 4, 4, 0)
